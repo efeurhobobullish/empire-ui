b6348014e5e4ad635d09b54548e0d00e5fe3de0b
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-06-08 15:15:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
@@ -5,14 +6,10 @@ const generateRoute = require("./routes/generate");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// File upload middleware
 const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use(express.json());
 app.use("/generate", upload.single("image"), generateRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
